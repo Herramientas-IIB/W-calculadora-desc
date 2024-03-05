23ef12307d07641cb33978e7bd9f79782bade866
Julian Baldovino <juliandbf14@gmail.com>
2024-03-05 18:21:40 -0500

    corrección

--- CalculadoraDescuentos/Form1.cs
+++ CalculadoraDescuentos/Form1.cs
@@ -18,13 +18,14 @@ namespace CalculadoraDescuentos
 
             try
             {
-                bool camposValidos1 = ((txtNom_Producto1.Text != "") && (float.Parse(txtPrecio_Producto1.Text) > 0) && (int.Parse(txtCant_Producto1.Text) > 0)) || txtCupon.Text != null;
+                bool camposValidos1 = ((txtNom_Producto1.Text != "") && (float.Parse(txtPrecio_Producto1.Text) > 0) && (int.Parse(txtCant_Producto1.Text) > 0));
+                bool camposValidos2 = ((txtNom_Producto2.Text != "") && (float.Parse(txtPrecio_Producto2.Text) > 0) && (int.Parse(txtCant_Producto2.Text) > 0));
                 Producto producto;
                 Producto producto2;
 
                 // TODO: Cupón debe ser OPCIONAL
 
-                if (camposValidos1)
+                if (camposValidos1 && !camposValidos2)
                 {
                     producto = new Producto(txtNom_Producto1.Text, float.Parse(txtPrecio_Producto1.Text), int.Parse(txtCant_Producto1.Text), txtCupon.Text);
 
@@ -37,15 +38,17 @@ namespace CalculadoraDescuentos
                 }
 
 
-                bool camposValidos2 = ((txtNom_Producto2.Text != "") && (float.Parse(txtPrecio_Producto2.Text) > 0) && (int.Parse(txtCant_Producto2.Text) > 0) || txtCupon.Text !=  null);
-
                 // TODO: Cupón debe ser OPCIONAL
 
-                if (camposValidos2)
+                if (camposValidos2 && !camposValidos1)
                 {
                     producto2 = new Producto(txtNom_Producto2.Text, float.Parse(txtPrecio_Producto2.Text), int.Parse(txtCant_Producto2.Text), txtCupon.Text);
                     float descuentoCantidad = 0;
                     descuentoCantidad = descuento.descuentoPorCantidad(producto2.Precio, producto2.Cantidad, producto2.Cupon);
+
+                    MessageBox.Show($"El nombre del producto es: {producto2.Nombre}\n" +
+                                    $"El precio original del producto es: {producto2.Precio}" +
+                                    $"\nEl descuento total es: {descuentoCantidad}");
                 }
 
                 if (camposValidos1 && camposValidos2)
@@ -56,6 +59,12 @@ namespace CalculadoraDescuentos
 
                     float descuentoPorCombinacion = 0;
                     descuentoPorCombinacion = descuento.descuentoCombinacionPrenda(producto, producto2, txtCupon.Text);
+
+                    MessageBox.Show($"El nombre del primer producto es: {producto.Nombre}\n" +
+                                    $"El precio original del primer producto es: {producto.Precio}\n" +
+                                    $"El nombre del segundo producto es: {producto2.Nombre}\n" +
+                                    $"El precio original del segundo producto es: {producto2.Precio}\n" +
+                                    $"\nEl descuento total es: {descuentoPorCombinacion}");
 
                 }
             }
